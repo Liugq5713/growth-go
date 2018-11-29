6ac3aace9379dc6f6b52bcb8e88696c420260126
liugq <liugq5713@gmail.com>
2018-11-29 18:30:39 +0800

    anonymouse func

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func compute(value int) {
+	for i := 0; i < value; i++ {
+		time.Sleep(time.Second)
+		fmt.Println(i)
+	}
+}
+
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("Goroutine Tutorial", time.Second)
+
+	go  compute(4)
+	go compute(4)
+	var input string
+	// Anonymous Goroutine Functions
+	go func() {
+		fmt.Println("Executing my Concurrent anonymouse function")
+	}()
+
+
+	fmt.Scanln(&input)
 }
