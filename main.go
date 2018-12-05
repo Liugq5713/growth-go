046f93b92c238e04e1d701e7f1f92a9580649648
liugq <liugq5713@gmail.com>
2018-12-05 14:49:38 +0800

    :leaves: 学习go  读写文件

--- main.go
+++ main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
+
+
+func readFile(){
+	data, err := ioutil.ReadFile("./test.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Print(string(data))
+}
+
+func writeFile() {
+	data := []byte("say hello to you")
+	err := ioutil.WriteFile("love.txt", data, 0777)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func appendFile(){
+	// 打开一个文件
+	f,err:= os.OpenFile("test.txt",os.O_APPEND|os.O_WRONLY,0600)
+	if err != err {
+		panic(err)
+	}
+	// 然后往里面写东西
+	if _,err= f.WriteString("new new new , i am new "); err!= nil {
+		panic(err)
+	}
+}
+
+
+
+
 func main() {
-	fmt.Println("hello world")
+	writeFile()
+	readFile()
+	appendFile()
 }
