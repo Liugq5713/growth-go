9def5c480cc49a74a3d191147c4bb711e3c9fcf9
liugq <liugq5713@gmail.com>
2018-12-05 15:07:44 +0800

    :construction:  read the input from console

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+func doSomeChoice(){
+	reader := bufio.NewReader(os.Stdin)
+	char,_,err := reader.ReadRune()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(char)
+	switch char {
+	case 'A':
+		fmt.Println("A key")
+		break
+	case 'B':
+		fmt.Println("B key")
+		break
+	}
+}
+
+func scanner() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
 func main() {
+	scanner()
+	doSomeChoice()
 	fmt.Println("hello world")
 }
