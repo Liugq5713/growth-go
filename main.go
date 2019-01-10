5af24cf8e8f7c9b0bff53a811408958cb6784335
liugq <liugq5713@gmail.com>
2019-01-10 11:22:49 +0800

    successfull to insert the db

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("Go MySQL Tutorial")
+	db, err := sql.Open("mysql", "root:123@tcp(127.0.0.1:3306)/test")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+	defer db.Close()
+
+	insert, err := db.Query("INSERT INTO `user` VALUES ( 3, 'sunc' )")
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// be careful deferring Queries if you are using transactions
+	defer insert.Close()
 }
